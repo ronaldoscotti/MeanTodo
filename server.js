b30ba0a65fab787287a410bf7fceef1332d8f48a
Ronaldo <ronaldoscottis@gmail.com>
2015-09-06 21:50:12 -0300

    Configurando server, mongo e rotas

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Setup
+// Setup Inicial
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
@@ -6,7 +6,8 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-// Config
+// Configurações
+
 mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
 
 app.use(express.static(__dirname + '/public'));
@@ -16,39 +17,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
-// Models
-var todo = mongoose.model('todo', {
+// Definindo Models
+var Todo = mongoose.model('Todo', {
 	text: String
 });
 
-// Routes 
+// Rotas
+
 // API
-// Get all todos
+// Busca todos os To-dos
 app.get('/api/todos', function (req, res) {
 
-	// Use mongoose to get all todos in the database
-	todo.find(function (err, todos) {
-		// if there is an error retrieving, send the error. nothing after res.send(err) will execute
+	// Usa mongoose para buscar todos os to-dos
+	Todo.find(function (err, todos) {
+
+		// Se ocorrer algum erro, receberá um retorno
 		if (err)
 			res.send(err)
-		
-		res.json(todos); // return all todos in JSON format
+			
+		// Retorna os to-dos em json
+		res.json(todos);
 	});
 });
 
-// Create todo and send back all todos after creation
+// Cria um to-do e envia de volta todos os outros
 app.post('/api/todos', function (req, res) {
 
-	// Create a todo, information comes from AJAX request from Angular
-	todo.create({
+	// Cria um to-do, a informação vem por ajax do Angular
+	Todo.create({
 		text: req.body.text,
 		done: false
 	}, function (err, todo) {
 		if (err)
 			res.send(err);
 
-		// get and return all the todos after you create another
-		todo.find(function (err, todos) {
+		// Se ocorrer algum erro, receberá um retorno
+		Todo.find(function (err, todos) {
 			if (err)
 				res.send(err)
 			res.json(todos);
@@ -57,16 +61,16 @@ app.post('/api/todos', function (req, res) {
 
 });
 
-// Delete a todo
+// Deleta um to-do
 app.delete('/api/todos/:todo_id', function (req, res) {
-	todo.remove({
+	Todo.remove({
 		_id: req.params.todo_id
 	}, function (err, todo) {
 		if (err)
 			res.send(err);
 
-		// get and return all the todos after you create another
-		todo.find(function (err, todos) {
+		// Retorna todos os to-dos restantes
+		Todo.find(function (err, todos) {
 			if (err)
 				res.send(err)
 			res.json(todos);
@@ -74,12 +78,6 @@ app.delete('/api/todos/:todo_id', function (req, res) {
 	});
 });
 
-// Application
-app.get('*', function(req, res) {
-	// Load the single view, Angular will take care of the other things
-	res.sendfile('./public/index.html');
-})
-
-// Starting server
+// Inicia o servidor na porta 3000
 app.listen(3000);
-console.log("Todo app running!");
+console.log("App rodando na porta 3000!");
